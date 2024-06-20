e88d167f240627026bfc504ce39e1c38b5cfd57c
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-06-20 17:05:58 +0800

    login form initial

--- src/components/auth/LoginForm.jsx
+++ src/components/auth/LoginForm.jsx
@@ -1,5 +1,3 @@
-// src/components/auth/LoginForm.jsx
-
 import React, { useState } from 'react';
 
 const LoginForm = () => {
@@ -15,35 +13,31 @@ const LoginForm = () => {
   return (
     <form onSubmit={handleSubmit} className="w-full max-w-sm">
       <div className="mb-4">
-        <label htmlFor="email" className="block text-sm font-bold mb-2">
-          Email:
-        </label>
         <input
           type="email"
           id="email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
           required
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          className="border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none placeholder-gray-400"
+          placeholder="Enter your email"
         />
       </div>
       <div className="mb-6">
-        <label htmlFor="password" className="block text-sm font-bold mb-2">
-          Password:
-        </label>
         <input
           type="password"
           id="password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
           required
-          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          className="border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none placeholder-gray-400"
+          placeholder="Enter your password"
         />
       </div>
       <div className="flex items-center justify-between">
         <button
           type="submit"
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className="bg-violet-500 hover:bg-violet-700 text-white font-bold py-2 px-4 rounded-sm focus:outline-none"
         >
           Login
         </button>
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,13 +1,13 @@
-// src/pages/auth/Login.jsx
-
 import React from 'react';
 import LoginForm from '../../components/auth/LoginForm';
 
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
-      <h1 className="text-3xl mb-4">Login</h1>
-      <LoginForm />
+      <div className="w-full max-w-md border-2 border-solid border-gray-150 py-10 px-5 md:py-20 md:px-20 rounded-lg">
+        <h1 className="text-3xl mb-4">Login</h1>
+        <LoginForm />
+      </div>
     </div>
   );
 };
