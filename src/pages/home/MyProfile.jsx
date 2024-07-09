b23614fa63b7245c1bd32c54d3b38d7f031fab59
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-07-09 20:15:28 +0800

    profile page modifyin

--- src/pages/home/MyProfile.jsx
+++ src/pages/home/MyProfile.jsx
@@ -23,25 +23,38 @@ const Profile = () => {
   };
 
   return (
-    <div className="min-h-screen bg-gray-100 pt-16 ">
+    <div className="min-h-screen bg-gray-100 pt-16">
       <Header /> {/* Your custom Header component */}
-      <div className="flex max-w-6xl mx-auto mt-8">
+      <div className="flex flex-col max-w-6xl mx-auto mt-8">
         {/* Profile Section */}
-        <div className="w-3/4 bg-white shadow-md rounded-lg overflow-hidden">
-          <div className="flex flex-col items-center p-11">
+        <div className="w-full bg-white shadow-md rounded-lg overflow-hidden">
+          <div className="relative">
             <img
-              className="h-80 w-80   rounded-full object-cover"
-              src="http://res.cloudinary.com/dihmqs39z/image/upload/v1717393349/ll3mgk5u2p1cvtzrjwyl.jpg"
-              alt="User Avatar"
+              className="w-full h-64 object-cover"
+              src="https://via.placeholder.com/1500x500" // Fallback cover photo URL
+              alt="Cover"
+              onError={(e) => {
+                e.target.src = "https://via.placeholder.com/1500x500"; // Fallback image URL in case of error
+              }}
             />
-            <div className="mt-4 text-center">
-              <h1 className="text-xl font-bold">User Name</h1>
-              <p className="text-gray-600">@username</p>
+            <div className="absolute top-40 left-4 transform -translate-y-1/2 z-10">
+              <img
+                className="h-32 w-32 rounded-full object-cover border-4 border-white"
+                src="http://res.cloudinary.com/dihmqs39z/image/upload/v1717393349/ll3mgk5u2p1cvtzrjwyl.jpg" // Profile photo URL
+                alt="User Avatar"
+              />
+            </div>
+            <div className="absolute top-64 left-44 transform -translate-y-1/2 z-10">
+              <div className="text-left">
+                <h1 className="text-2xl font-bold">User Name</h1>
+                <p className="text-gray-600">@username</p>
+                <p className="text-gray-600">2.5K friends</p>
+              </div>
             </div>
           </div>
         </div>
         {/* Posts Section */}
-        <div className="w-3/4 ml-8">
+        <div className="w-full mt-8">
           <div className="bg-white shadow-md rounded-lg overflow-hidden">
             <form onSubmit={handlePostSubmit} className="px-6 py-4">
               <textarea
