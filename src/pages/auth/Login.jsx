79f7f55388340805800bc864bfb3dda1dec507a6
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-07-23 03:10:17 +0800

    modifying login page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -5,7 +5,7 @@ import logo from '../../assets/logo/logos.png';
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
-      <div className="w-full max-w-md border-2 border-solid border-gray-150 py-10 px-5 rounded-lg flex flex-col items-center">
+      <div className="w-full max-w-md border-0 md:border-2 border-solid border-gray-150 py-10 px-5 rounded-lg flex flex-col items-center ">
         <img src={logo} alt="Feelink Logo" className="mx-auto mt-5 mb-5 w-20 h-20" /> 
         <LoginForm />
       </div>
