56a14826b1d0dd95289414202c3c661a568b2dd4
JohnmackFaeldonia <johmmackfaeldonia@gmail.com>
2024-08-17 09:08:47 +0800

    Implement timestamp formatting function for posts
    - Added ormatTimestamp function to format timestamps into human-readable strings.
    - Handles various time differences: 'Just now', '1m ago', 'X minutes ago', 'Today at X:XX', 'Day of the week at X:XX', and 'Month Day, Year at X:XX'.

--- src/utils/dateUtils.js
+++ src/utils/dateUtils.js
@@ -1,25 +1,23 @@
 export const formatTimestamp = (timestamp) => {
-    const now = new Date();
-    const postDate = new Date(timestamp);
-    const diff = now - postDate;
-    const seconds = Math.floor(diff / 1000);
-    const minutes = Math.floor(seconds / 60);
-    const hours = Math.floor(minutes / 60);
-    const days = Math.floor(hours / 24);
+  const now = new Date();
+  const postDate = new Date(timestamp);
+  const diff = now - postDate;
+  const seconds = Math.floor(diff / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
 
-    if (seconds < 60) {
-      return 'Just now';
-    } else if (seconds < 120) {
-      return '1m ago';
-    } else if (minutes < 60) {
-      return `${minutes}m ago`;
-    } else if (hours < 24 && now.getDate() === postDate.getDate()) {
-      return postDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    } else if (hours < 24 && now.getDate() !== postDate.getDate()) {
-      return postDate.toLocaleString('en-US', { weekday: 'short', hour: 'numeric', minute: 'numeric' });
-    } else if (days < 7) {
-      return postDate.toLocaleString('en-US', { month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' });
-    } else {
-      return postDate.toLocaleString('en-US', { month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' });
-    }
-  };
+  if (seconds < 60) {
+    return 'Just now';
+  } else if (seconds < 120) {
+    return '1m ago';
+  } else if (minutes < 60) {
+    return `${minutes}m ago`;
+  } else if (hours < 24 && now.getDate() === postDate.getDate()) {
+    return postDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+  } else if (days < 7) {
+    return postDate.toLocaleString('en-US', { weekday: 'short', hour: 'numeric', minute: 'numeric' });
+  } else {
+    return postDate.toLocaleString('en-US', { month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' });
+  }
+};
